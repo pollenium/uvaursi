9e2de77d4dc614c3257157e3091f2a757a47ee41
Aakil Fernandes <aakilfernandes@gmail.com>
2019-11-19 16:27:57 -0500

    fromArray errors

---
+++ ts/src/fromArray.test.ts
@@ -0,0 +1,23 @@
+import { fromArray, NegativeNumberError, DecimalNumberError, TooLargeNumberError } from './fromArray'
+
+test('[0, 255]', () => {
+  expect(fromArray([0, 255])).toStrictEqual(new Uint8Array([0, 255]))
+})
+
+test('NegativeNumberError', () => {
+  expect(() => {
+    fromArray([-1])
+  }).toThrow(NegativeNumberError)
+})
+
+test('DecimalNumberError', () => {
+  expect(() => {
+    fromArray([1.5])
+  }).toThrow(DecimalNumberError)
+})
+
+test('TooLargeNumberError', () => {
+  expect(() => {
+    fromArray([256])
+  }).toThrow(TooLargeNumberError)
+})
--- ts/src/fromArray.ts
+++ ts/src/fromArray.ts
@@ -1,4 +1,35 @@
+export class NegativeNumberError extends Error {
+  constructor(number) {
+    super(`Negative number: ${number}`)
+    Object.setPrototypeOf(this, NegativeNumberError.prototype)
+  }
+}
+
+export class DecimalNumberError extends Error {
+  constructor(number) {
+    super(`Decimal number: ${number}`)
+    Object.setPrototypeOf(this, DecimalNumberError.prototype)
+  }
+}
+
+export class TooLargeNumberError extends Error {
+  constructor(number) {
+    super(`Too large number: ${number} is greater than 255`)
+    Object.setPrototypeOf(this, TooLargeNumberError.prototype)
+  }
+}
+
 export function fromArray(array: Array<number>): Uint8Array {
-  /* ToDo: Validate uintish number */
+  array.forEach((number) => {
+    if (number < 0) {
+      throw new NegativeNumberError(number)
+    }
+    if (number % 1 > 0) {
+      throw new DecimalNumberError(number)
+    }
+    if (number > 255) {
+      throw new TooLargeNumberError(number)
+    }
+  })
   return new Uint8Array(array)
 }
